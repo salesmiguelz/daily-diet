04fd20921090c2370c783489909396fb26cc4b58
Miguel Sales <miguelsales02@gmail.com>
2024-10-20 12:35:37 -0300

    feat: added zod and created list meals and create meals routes

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,28 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import knex from '../database/database-connection'
+import { randomUUID } from 'crypto'
 
 export default async (app: FastifyInstance) => {
-  app.get('/', (request, reply) => {
-    reply.send('Hello!')
+  app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    const meals = await knex('meals').select('*')
+
+    return reply.send({ meals })
+  })
+
+  app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    const meal = z.object({
+      name: z.string(),
+      dateAndTime: z.string().datetime(),
+      isWithinDiet: z.boolean(),
+    })
+    const { name, dateAndTime, isWithinDiet } = meal.parse(request.body)
+    await knex('meals').insert({
+      id: randomUUID(),
+      name,
+      dateAndTime,
+      isWithinDiet,
+    })
+    return reply.send(201)
   })
 }
