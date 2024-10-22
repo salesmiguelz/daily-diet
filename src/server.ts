beea66a55dea607598d1de522b8440500b4014bc
Miguel Sales <miguelsales02@gmail.com>
2024-10-22 16:08:56 -0300

    feat: added .env configs

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
+import { env } from './env/index'
+import 'dotenv/config'
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is up and running!')
