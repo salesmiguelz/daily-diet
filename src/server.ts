558c6bce01467375a1963b05fb78b7d40e80a48b
Miguel Sales <miguelsales02@gmail.com>
2024-10-22 23:24:19 -0300

    fix: add render port on server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import 'dotenv/config'
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
   })
   .then(() => {
     console.log('Server is up and running!')
